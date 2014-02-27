aa663465e05a067f949170b61b29b57d426b5e0f
jservadio <joseph_servadio@brown.edu>
2014-02-27 13:22:15 -0500

    Update treat.diff.change.R

--- R/treat.diff.change.R
+++ R/treat.diff.change.R
@@ -1,8 +1,9 @@
-treat.diff.change <-
-function (treat.diff, score.range) 
+treat.diff.change <- function (treat.diff, score.range) 
 {
     treat.diff.change = treat.diff/score.range
-    treat.diff.change.1 = as.vector(c(quantile(treat.diff.change, 0.025), median(treat.diff.change), quantile(treat.diff.change, 0.975)))
-    treat.diff.change.2 = as.numeric(table(cut(treat.diff.change, breaks = c(-1, -0.2, 0, 0.2, 1))))/length(treat.diff.change)
+    treat.diff.change.1 = as.vector(c(quantile(treat.diff.change, 0.025), median(treat.diff.change), 
+		quantile(treat.diff.change, 0.975)))
+    treat.diff.change.2 = as.numeric(table(cut(treat.diff.change, breaks = 
+		c(-1, -0.2, 0, 0.2, 1))))/length(treat.diff.change)
     return(list(treat.diff.change.1, treat.diff.change.2))
 }
