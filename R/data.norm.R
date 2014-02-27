5dda78e1fd2c75c431300e0d2eb25a58fe27ea0f
jservadio <joseph_servadio@brown.edu>
2014-02-27 13:20:11 -0500

    Update data.norm.R

--- R/data.norm.R
+++ R/data.norm.R
@@ -1,8 +1,7 @@
-data.norm <-
-function (Y, Covs, prior, Treat) 
+data.norm <- function (Y, Covs, prior, Treat) 
 {
-    inData = list(Treat = Treat, Y = Y, mean.alpha = prior$mean.alpha, prec.alpha = prior$prec.alpha, mean.beta = prior$mean.beta, 
-        prec.beta = prior$prec.beta, prec.1 = prior$prec.1, prec.2 = prior$prec.2)
+    inData = list(Treat = Treat, Y = Y, mean.alpha = prior$mean.alpha, prec.alpha = prior$prec.alpha, mean.beta = 
+		prior$mean.beta, prec.beta = prior$prec.beta, prec.1 = prior$prec.1, prec.2 = prior$prec.2)
     if (!is.null(Covs)) {
         inData[[1 + length(inData)]] = Covs
         names(inData)[[length(inData)]] = "x"
