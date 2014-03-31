687156513700c206f139f300e84803b8c4b4fb8b
jservadio <joseph_servadio@brown.edu>
2014-03-31 15:34:42 -0400

    Update run.norm.R

--- R/run.norm.R
+++ R/run.norm.R
@@ -1,29 +1,25 @@
-run.norm <- function (Y, Treat, Covs, model, var.model, nChains = 3, conv.limit = 1.05, niters = 50000, nruns = 5000, 
-    setsize = 4000, slopeprior, alphaprior, betaprior, varprior, varprior.params, path) 
+run.norm <-
+function (Y, Treat, Covs, model, var.model, nChains = 3, conv.limit = 1.05, niters = 50000, nruns = 5000,
+    setsize = 4000, slopeprior, alphaprior, betaprior, varprior, varprior.params, path, insuf)
 {
-
-	stopifnot(max(Y) > min(Y))
-	
     nobs = length(Y)
-    if (!is.null(Covs)) 
+	
+	stopifnot(insuf == FALSE)
+	
+    if (!is.null(Covs))
         Covs = as.matrix(Covs)
     prior = prior.norm(Covs, alphaprior, betaprior, slopeprior, varprior, varprior.params)
-    inInitials = inits.norm(Y, Covs, Treat, varprior, ntreat = 2)
-    inInits = list(inInitials[[1]], inInitials[[2]], inInitials[[3]])
-	Significant = inInitials[[4]]
-	
-	inData <- data.norm(Y, Covs, prior, Treat)
+    inInits = inits.norm(Y, Covs, Treat, varprior, ntreat = 2)
+    inData <- data.norm(Y, Covs, prior, Treat)
     model.norm(nobs, Covs, prior, varprior, path)
     pars.to.save <- c("alpha", "beta")
-    if (!is.null(Covs)) 
+    if (!is.null(Covs))
         pars.to.save = c(pars.to.save, "slope")
     pars.to.save = c(pars.to.save, "Sd")
-    jags.out <- jags.fit(inData, inInits, pars.to.save, model, "model.txt", nChains, niters, conv.limit, 
-		setsize, nruns=5000, Covs)
+    jags.out <- jags.fit(inData, inInits, pars.to.save, model, "model.txt", nChains, niters, conv.limit, setsize, nruns, Covs)
     burn.in <- jags.out[[1]]
     no.runs <- jags.out[[2]]
     samples <- jags.out[[3]]
-	DIC = jags.out[[4]]
     varnames <- dimnames(samples)[[3]]
     nvars <- dim(samples)[3]
     alpha.vars <- grep("alpha", varnames)
@@ -37,12 +33,12 @@ run.norm <- function (Y, Treat, Covs, model, var.model, nChains = 3, conv.limit 
         slope <- samples[, , slope.vars]
     }
     if (is.null(Covs)) {
-        out <- list(burn.in, no.runs, Y, alpha, beta, Sd, DIC)
-        names(out) <- c("Burn In", "Number runs per chain", "Y", "alpha", "beta", "Sd", "DIC")
+        out <- list(burn.in, no.runs, Y, alpha, beta, Sd)
+        names(out) <- c("Burn In", "Number runs per chain", "Y", "alpha", "beta", "Sd")
     }
     else {
-        out <- list(burn.in, no.runs, Y, alpha, beta, Sd, slope, DIC)
-        names(out) <- c("Burn In", "Number runs per chain", "Y", "alpha", "beta", "Sd", "Slopes", "DIC")
+        out <- list(burn.in, no.runs, Y, alpha, beta, Sd, slope)
+        names(out) <- c("Burn In", "Number runs per chain", "Y", "alpha", "beta", "Sd", "Slopes")
     }
     return(out)
 }
