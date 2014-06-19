91b670f8d97d1cb15e5054921e81ad19c609f8e1
jservadio <joseph_servadio@brown.edu>
2014-06-19 16:10:18 -0400

    Update treat.diffs.R

--- R/treat.diffs.R
+++ R/treat.diffs.R
@@ -1,6 +1,15 @@
 treat.diffs <- function(p, Treat)
 {
 	expect.score = apply(p,1:2, function(x) sum(x*seq(length(x)))) #expected score matrix dim nsims x nobs
-	treat.diff = apply(expect.score,1, function(x,Treat) sum(x*(2*Treat-1))/(length(x)/2), Treat) #dim = nsims
+	
+	expect.score.A = expect.score[ , Treat == 0]
+	expect.score.B = expect.score[ , Treat == 1]
+	
+	mean.A = rowMeans(expect.score.A)
+	mean.B = rowMeans(expect.score.B)
+	
+	n.A = dim(expect.score.A)[2]
+	n.B = dim(expect.score.B)[2]
+	
+	treat.diff = mean.B / n.B - mean.A / n.A
 }
-
