9eddeda1f276221738851f3e0aae50e5d7310943
jservadio <joseph_servadio@brown.edu>
2014-06-19 16:11:32 -0400

    Update model.norm.R

--- R/model.norm.R
+++ R/model.norm.R
@@ -1,7 +1,7 @@
-model.norm <- function (nobs, Covs, prior, varprior, path, i) 
+model.norm <- function (nobs, Covs, prior, varprior, path, mod.id) 
 {
     cat("model\n{", file = paste(path, "model.txt", sep = ""))
-    model.norm.obs(nobs, Covs, prior, path, i)
+    model.norm.obs(nobs, Covs, prior, path, mod.id)
     var.hom(prior, varprior, path)
     cat("\n    }", file = paste(path, "model.txt", sep = ""), append = T, sep = "")
 }
