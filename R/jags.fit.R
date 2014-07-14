6d02e1d563c5299b03e1d7a6804f8619c4f57ca4
jservadio <joseph_servadio@brown.edu>
2014-07-14 10:18:50 -0400

    Update jags.fit.R

--- R/jags.fit.R
+++ R/jags.fit.R
@@ -1,4 +1,3 @@
-
 jags.fit <- function (inData, inInits, pars.to.save, model, model.file, n.chains, niters, conv.limit, 
 	setsize, nruns=5000, Covs) 
 {
@@ -81,3 +80,13 @@ jags.fit <- function (inData, inInits, pars.to.save, model, model.file, n.chains
     return(out)
 }
 
+
+treat.diff.change <- function (treat.diff, score.range) 
+{
+    treat.diff.change = treat.diff/score.range
+    treat.diff.change.1 = as.vector(c(quantile(treat.diff.change, 0.025), median(treat.diff.change), 
+		quantile(treat.diff.change, 0.975)))
+    treat.diff.change.2 = as.numeric(table(cut(treat.diff.change, breaks = 
+		c(-1, -0.2, 0, 0.2, 1))))/length(treat.diff.change)
+    return(list(treat.diff.change.1, treat.diff.change.2))
+}
