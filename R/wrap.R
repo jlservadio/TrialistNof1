af2d46278dc2b3be468e0d3baf072ff70c610fb4
jservadio <joseph_servadio@brown.edu>
2014-10-02 16:11:04 -0400

    Update wrap.R

--- R/wrap.R
+++ R/wrap.R
@@ -2702,7 +2702,8 @@ wrap <- function(data, metadata) {
 	meta.data[[length(meta.data) + 1]] = Best.Model
 	names(meta.data)[length(meta.data)] = "Best.Model"
 
-
+	meta.data[[length(meta.data) + 1]] = "Oct. 2 2014, 16:10"
+	names(meta.data)[length(meta.data)] = "Update Time"
 	
 	out[[length(out) + 1]] = meta.data
 	names(out)[length(out)] = "meta.data"
