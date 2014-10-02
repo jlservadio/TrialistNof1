801b55143c17820aa9a4c6783eddda351ad12841
jservadio <joseph_servadio@brown.edu>
2014-10-02 16:00:04 -0400

    Update treat.diffs.R

--- R/treat.diffs.R
+++ R/treat.diffs.R
@@ -10,6 +10,8 @@ treat.diffs <- function(p, Treat)
 	
 	n.A = dim(expect.score.A)[2]
 	n.B = dim(expect.score.B)[2]
+	
+#	treat.diff = mean.B / n.B - mean.A / n.A
 
 	treat.diff = mean.B - mean.A
 }
